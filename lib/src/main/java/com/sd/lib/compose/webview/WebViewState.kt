d091af99f50b198ee77a33c464908696d2b9952d
zj565061763 <565061763@qq.com>
2022-12-26 12:09:29 +0800

    simplify

--- lib/src/main/java/com/sd/lib/compose/webview/WebViewState.kt
+++ lib/src/main/java/com/sd/lib/compose/webview/WebViewState.kt
@@ -56,31 +56,41 @@ open class FWebViewState : WebViewState() {
         this.content = content
     }
 
-    fun evaluateJavascriptFunction(function: String, callback: ValueCallback<String?>? = null) {
-        if (function.isEmpty()) return
-        val script = "$function()"
-        evaluateJavascript(script, callback)
-    }
-
-    fun evaluateJavascriptFunction(function: String, params: Array<Any>, callback: ValueCallback<String?>? = null) {
-        if (function.isEmpty()) return
-        val paramsString = params.joinToString(
-            separator = ",",
-            transform = {
-                if (it is String) {
-                    "'${it}'"
-                } else {
-                    it.toString()
-                }
-            }
-        )
-        val script = "$function($paramsString)"
-        evaluateJavascript(script, callback)
-    }
-
-    fun evaluateJavascript(script: String, callback: ValueCallback<String?>? = null) {
-        webView?.evaluateJavascript(script, callback)
-    }
-
     protected open fun onUpdate(view: WebView) {}
+}
+
+fun WebView.fEvaluateJavascriptFunction(
+    function: String,
+    callback: ValueCallback<String?>? = null,
+) {
+    if (function.isEmpty()) return
+    val script = "$function()"
+    evaluateJavascript(script, callback)
+}
+
+fun WebView.fEvaluateJavascriptFunction(
+    function: String,
+    params: Array<Any?>,
+    callback: ValueCallback<String?>? = null,
+) {
+    if (function.isEmpty()) return
+    val paramsString = params.joinToString(
+        separator = ",",
+        transform = {
+            if (it is String) {
+                "'${it}'"
+            } else {
+                it.toString()
+            }
+        }
+    )
+    val script = "$function($paramsString)"
+    fEvaluateJavascript(script, callback)
+}
+
+fun WebView.fEvaluateJavascript(
+    script: String,
+    callback: ValueCallback<String?>? = null,
+) {
+    this.evaluateJavascript(script, callback)
 }
