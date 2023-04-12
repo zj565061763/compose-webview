218edc1ca48def4993332f38670515a03bf69026
zj565061763 <565061763@qq.com>
2023-04-12 23:06:24 +0800

    1.0.0-alpha04

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "compose-webview"
-val libVersionName = "1.0.0-alpha03"
+val libVersionName = "1.0.0-alpha04"
 
 android {
     namespace = "com.sd.lib.compose.webview"
